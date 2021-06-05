74c0b98a5a82067e4977c54e21ca0758e62fb174
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-05 16:39:04 +0300

    Adds test for source class behaviour

--- app/news_test.py
+++ app/news_test.py
@@ -16,6 +16,19 @@ class ArticleTest(unittest.TestCase):
         
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Article))
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test class to test the source class behaviour
+    '''
+    def setUp(self):
+        '''
+        Method to run before test
+        '''
+        self.new_source = Source("Benard","The palestinian War","Been going on for long","https://mw3.wsj.net/mw5/content/logos/mw_logo_social.png","2021-12-03","Nyatanyau has had his full force felt",)
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
     
     if __name__ == '__main__':
      unittest.main()
