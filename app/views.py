90831d2b95ab49aba269f4686fa56f633b671272
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-06-05 20:57:53 +0300

    Sources news using news api

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 #Views
 @app.route('/')
@@ -7,5 +8,8 @@ def index():
     '''
     Function that returns the index page and its data
     '''
+    #getting news
+    get_news = get_sources()
+    print(get_news)
     title = 'Home - News Site'
     return render_template('index.html', title = title)
